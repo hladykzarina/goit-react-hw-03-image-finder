96f95df511b194bda997102545c5735e3c3f4a7a
bogunenko-daryna <124713486+hladykzarina@users.noreply.github.com>
2023-10-17 20:45:40 +0200

    loader

---
+++ src/components/App.styled.jsx
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const AppContent = styled.div`
+  display: grid;
+  justify-items: center;
+  grid-template-columns: 1fr;
+  grid-gap: 16px;
+  padding-bottom: 24px;
+`;
